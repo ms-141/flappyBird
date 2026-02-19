0c072dc21db1ae0bd48ab5df930152b93b66abde
Declan878 <dmcca878@mtroyal.ca>
2026-02-19 16:34:12 -0700

    changed the fields of the pipes

--- set_of_pipes.h
+++ set_of_pipes.h
@@ -3,8 +3,8 @@
 
 typedef struct
 {
-    unsigned int x; // each set of pipes has a continuously changing horizontal pos.
-    unsigned int y; // the vertical pos. of the bottom of the top pipe
+    unsigned int x, // each set of pipes has a continuously changing horizontal pos.
+                 y; // the vertical pos. of the bottom of the top pipe
     unsigned int gap_size; // the size of the gap between the top and bottom pipe
                            // used to determine the vertical pos. of the top of the bottom pipe                 
 } SetOfPipes;
