7881b139317927d3d2390571bbb169d6d25ef954
Declan878 <dmcca878@mtroyal.ca>
2026-02-19 20:50:30 -0700

    added bird behaviour functions, changed bird fields and added constants

--- bird.c
+++ bird.c
@@ -1,1 +1,32 @@
 #include "bird.h"
+
+void birdJump(Bird *bird) {
+    bird->y += BIRD_JUMP_DELTA; /* change vertical pos. by the jump delta */
+}
+
+void birdFalling(Bird *bird) {
+    bird->y += BIRD_FALL_DELTA; /* change vertical pos. by the fall delta */
+}
+
+void update_position(Bird *bird, int jump_pressed) {
+    if (jump_pressed) 
+        birdJump(bird); /* if the jump button is pressed, make the bird jump */
+    else 
+        birdFalling(bird); /* otherwise, make the bird fall */
+}
+
+int checkCollision(Bird *bird, SetOfPipes *pipes, unsigned int ground_height) {
+    if (bird->y >= ground_height) 
+        return 1; /* collision with the ground */
+
+    if (bird->y <= 0) 
+        return 1; /* collision with the top of the screen */
+    
+    if (bird->x + BIRD_WIDTH >= pipes->x && bird->x <= pipes->x + PIPE_WIDTH) {
+        if (bird->y <= pipes->y || bird->y + BIRD_HEIGHT >= pipes->y + PIPE_GAP_SIZE) {
+            return 1; /* Collision with the pipes */
+        }
+    }
+
+    return 0; /* No collision */
+}
--- bird.h
+++ bird.h
@@ -1,11 +1,26 @@
 #ifndef BIRD_H
 #define BIRD_H
 
+#define BIRD_X_POS 50
+#define BIRD_JUMP_DELTA -45 /* bird will move up by this many pixels upon user input */
+#define BIRD_FALL_DELTA 1 /* bird will move down by this many pixels after each clock tick when not jumping */
+#define BIRD_WIDTH 16
+#define BIRD_HEIGHT 16
+
 typedef struct
 {
-    unsigned int y; /* bird only changes position vertically */
-
-    int delta_y; /* how much position changes after a clock tick */
+    unsigned int x, y; /* bird's horizontal pos. is fixed but
+                       the birds vertical pos. changes */
 } Bird;
+
+/* makes the bird jump upwards by subtracting from its y pos. */
+void birdJump(Bird *bird); 
+/* makes the bird fall downwards by adding to its y pos. */  
+void birdFalling(Bird *bird); 
+/* uses birdJump and birdFalling to update the bird's position */
+void update_position(Bird *bird, int jump_pressed);
+/* checks if the bird has collided with the pipes, the ground, or the top of the screen (y = 0) 
+1 = collided, 0 = hasn't collided */
+int checkCollision(Bird *bird, SetOfPipes *pipes, unsigned int ground_height);
 
 #endif
