bcb07ec871554edbd022247defbb857b1f2daf95
Gurshan <gcher483@mtroyal.ca>
2026-02-19 17:58:07 -0700

    created score.h file

---
+++ score.h
@@ -0,0 +1,10 @@
+#ifndef SCORE_H
+#define SCORE_H
+
+typedef struct
+{
+	unsigned int curr_score; /* current score for the current round */
+	unsigned int high_score; /* highest score out of all of the rounds */
+} Score;
+
+#endif
