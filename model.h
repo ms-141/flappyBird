37efa289ba5ea1814873ead8dbdb0b3fc47c7027
Declan878 <dmcca878@mtroyal.ca>
2026-02-19 18:27:49 -0700

    added score object files to Model

--- model.h
+++ model.h
@@ -2,11 +2,13 @@
 #define MODEL_H
 #include "bird.h"
 #include "set_of_pipes.h"
+#include "score.h"
 
 typedef struct
 {
     Bird bird;
     SetOfPipes pipes[3]; /* 3 sets of pipes will be on the screen at any time */
+    Score score;
 } Model;
 
 #endif
