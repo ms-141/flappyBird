a8a67383ab98c978c55123d0f90af31bf3333855
Declan878 <dmcca878@mtroyal.ca>
2026-02-19 17:48:00 -0700

    Added behaviour functions, fixed comment style, changed pipe fields, and added constants

--- set_of_pipes.c
+++ set_of_pipes.c
@@ -1,1 +1,14 @@
 #include "set_of_pipes.h"
+
+void movePipes(SetOfPipes *pipes, int delta_x) {
+    pipes->x -= delta_x; /* Move the pipes to the left by delta_x pixels */
+}
+
+void resetPipes(SetOfPipes *pipes, unsigned int new_x, unsigned int new_y) {
+    pipes->x = new_x; /* Resets the horizontal position of the pipes to new_x */
+    pipes->y = new_y; /* Resets the vertical position of the bottom of the top pipe to new_y */
+}
+
+int isOffScreen(SetOfPipes *pipes, unsigned int screen_width) {
+    return (pipes->x + PIPE_WITDH) < 0; /* returns true if the right edge of the pipes is off screen */
+}
--- set_of_pipes.h
+++ set_of_pipes.h
@@ -1,12 +1,23 @@
 #ifndef SET_OF_PIPES_H
 #define SET_OF_PIPES_H
 
+#define PIPE_WITDH 32
+#define PIPE_GAP_SIZE 100
+
 typedef struct
 {
-    unsigned int x, // each set of pipes has a continuously changing horizontal pos.
-                 y; // the vertical pos. of the bottom of the top pipe
-    unsigned int gap_size; // the size of the gap between the top and bottom pipe
-                           // used to determine the vertical pos. of the top of the bottom pipe                 
+    int x; /* each set of pipes has a continuously changing horizontal pos. */
+    unsigned int y; /* the vertical pos. of the bottom of the top pipe.
+                    the pipe gap size will be added to this value to 
+                    determine the vertical pos. of the top of the bottom pipe */
+                
 } SetOfPipes;
+
+/* moves the pipes by the given speed (delta_x) */
+void movePipes(SetOfPipes *pipes, int delta_x); 
+/* deletes pipes that are off screen and spawns new pipes with a new height */
+void resetPipes(SetOfPipes *pipes, unsigned int new_x, unsigned int new_y); 
+/* checks if the pipes are off screen (x < 0) */
+int isOffScreen(SetOfPipes *pipes, unsigned int screen_width);
 
 #endif
