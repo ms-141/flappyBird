181396c2cf24769bd735a0b499f9c6b4f57fb903
Declan878 <dmcca878@mtroyal.ca>
2026-02-21 00:14:24 -0700

    added additional field to bird to allow jumping over 3 ticks

--- bird.h
+++ bird.h
@@ -3,7 +3,7 @@
 
 #define BIRD_VERT_START_POS 200
 #define BIRD_X_POS 50
-#define BIRD_JUMP_DELTA -45 /* bird will move up by this many pixels upon user input */
+#define BIRD_JUMP_DELTA -15 /* bird will move up by this many pixels upon user input for 3 ticks (45 tot) */
 #define BIRD_FALL_DELTA 1 /* bird will move down by this many pixels after each clock tick when not jumping */
 #define BIRD_WIDTH 16
 #define BIRD_HEIGHT 16
@@ -12,6 +12,7 @@ typedef struct
 {
     unsigned int x; /* bird's horizontal pos. is fixed */
     int y; /* the birds vertical pos. changes */
+    unsigned int jump_ticks_remaining; /* the number of ticks remaining in the current jump */
 } Bird;
 
 /* initializes the bird's position at the start of the game */
@@ -20,8 +21,6 @@ void spawnBird(Bird *bird);
 void birdJump(Bird *bird); 
 /* makes the bird fall downwards by adding to its y pos. */  
 void birdFalling(Bird *bird); 
-/* uses birdFalling to update the bird's position */
-void updatePosition(Bird *bird);
 /* checks if the bird has collided with the pipes, the ground, or the top of the screen (y = 0) 
 1 = collided, 0 = hasn't collided */
 int checkCollision(Bird *bird, SetOfPipes *pipes, unsigned int ground_height);
--- model.c
+++ model.c
@@ -2,10 +2,12 @@
 
 void modelInit(Model *model) {
     unsigned int i;
+    unsigned int variable_distance = 0; /* the distance between each set of pipes */
 
     spawnBird(&model->bird);
     for (i = 0; i < 3; i++) {
-        resetPipes(&model->pipes[i], 200 + i * 150, 200); /* the height is currently not random */
+        resetPipes(&model->pipes[i], 200 + variable_distance, 200); /* the height is currently not random */
+        variable_distance += 150; /* increase the distance for the next set of pipes */
     }
     model->score.curr_score = 0;
 }
