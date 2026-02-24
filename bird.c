d139bd67c17743eddd1ff78cd86025aad8354037
Declan878 <dmcca878@mtroyal.ca>
2026-02-23 19:29:59 -0700

    fixed a problem where the jump_ticks_remaining field for the bird wasn't being initialized to 0

--- bird.c
+++ bird.c
@@ -15,6 +15,7 @@
 void spawnBird(Bird *bird) {
     bird->x = BIRD_X_POS; /* set the bird's horizontal pos. to the defined constant */
     bird->y = BIRD_VERT_START_POS; /* set the bird's vertical pos. to the defined constant */
+    bird->jump_ticks_remaining = 0; /* set the bird's jump ticks remaining to 0 at the start of the game */
 }
 
 void birdJump(Bird *bird) {
