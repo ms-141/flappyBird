9f9985d35a1e837cd4db531e2013dd93cda89add
Declan878 <dmcca878@mtroyal.ca>
2026-02-26 18:17:18 -0700

    adding file (empty)

---
+++ raster.c
@@ -0,0 +1,11 @@
+/*
+ * File: raster.c
+ *
+ * Team Members: Muji Shah, Gurshan Chera, Declan McCarthy
+ * Date Created: February 23, 2026
+ *
+ * Description:
+ * This file implements the functions for the Raster routines.
+ *
+ * File Status:             
+*/ 
