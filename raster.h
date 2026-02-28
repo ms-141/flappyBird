8d901272ea14e9f7e0a542c688f74b26486a372b
Gurshan <gcher483@mtroyal.ca>
2026-02-27 17:49:50 -0700

    fixed typo in raster.h plot_vertical_line description

--- raster.h
+++ raster.h
@@ -77,7 +77,7 @@ void plot_horizontal_line(UINT32 *base, int row, int col, UINT16 length);
 
 /*----- Function: plot_vertical_line -----
 
- PURPOSE: Plot a hoizontal line on the screen. The vertical line is specified by the topmost pixel of the line and the length of the line.
+ PURPOSE: Plot a vertical line on the screen. The vertical line is specified by the topmost pixel of the line and the length of the line.
 
  INPUT: Address(UINT32*): to the start of the screen
         Position(row,col): the coordinates of the topmost pixel of the vertical line
