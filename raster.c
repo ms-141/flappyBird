88fea8be312216020bb49c86d106cc91223c2f83
Declan878 <dmcca878@mtroyal.ca>
2026-02-27 20:05:55 -0700

    added clear_region raster function

--- raster.c
+++ raster.c
@@ -21,6 +21,47 @@ void clear_screen(UINT32 *base)
         *(loc++) = 0;
 }
 
+void clear_region(UINT32 *base, int row, int col, UINT16 length, UINT16 width)
+{
+    int r, c; /* r = row, c = column */
+
+    int start_row = row;
+    int start_col = col;
+    int end_row = row + length;
+    int end_col = col + width;
+
+    /* completely off-screen (return) */
+    if (end_row <= 0 || end_col <= 0 ||
+        start_row >= SCREEN_HEIGHT ||
+        start_col >= SCREEN_WIDTH)
+        return;
+
+    /* top is clipped */
+    if (start_row < 0) 
+        start_row = 0;
+    /* left is clipped */
+    if (start_col < 0) 
+        start_col = 0;
+    /* right is clipped */
+    if (end_row > SCREEN_HEIGHT) 
+        end_row = SCREEN_HEIGHT;
+    /* bottom is clipped */
+    if (end_col > SCREEN_WIDTH)  
+        end_col = SCREEN_WIDTH;
+
+    for (r = start_row; r < end_row; r++)
+    {
+        unsigned int row_offset = (r << 6) + (r << 4); /* r * 80 */
+
+        for (c = start_col; c < end_col; c++)
+        {
+            UINT8 *byte = (UINT8 *)base + row_offset + (c >> 3);
+
+            *byte &= ~(1 << (7 - (c & 7)));  /* clear pixel */
+        }
+    }
+}
+
 void plot_pixel(UINT8 *base, int row, int col) {
     unsigned int row_offset;
     if (col >= 0 && col < SCREEN_WIDTH && 
