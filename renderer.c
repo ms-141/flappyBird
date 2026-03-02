63d418226284ba6a5ba4671839566a4fde39fc1d
Muji <128879165+ms-141@users.noreply.github.com>
2026-03-01 20:52:16 -0700

    uncommented function call

--- renderer.c
+++ renderer.c
@@ -7,29 +7,28 @@
  * Description:
  * This file implements the functions for the Renderer routines.
  *
- * File Status: 
+ * File Status:
  * There is no bird_bitmap currently defined.
- * 
+ *
  * render_pipe will not work until the plot_rectangle is implemented
- * 
+ *
  * render_score will not work until the plot_string is implemented
-*/
+ */
 
 #include "renderer.h"
 
-UINT16 bird_bitmap [16] = {
-	0x0000, 0x0000, 0x03E0, 0x0C10,
-	0x1878, 0x3088, 0x608C, 0x409E,
-	0x4073, 0x4001, 0x7007, 0x1004,
-	0x180C, 0x0C18, 0x07F0, 0x0000
-};
+UINT16 bird_bitmap[16] = {
+    0x0000, 0x0000, 0x03E0, 0x0C10,
+    0x1878, 0x3088, 0x608C, 0x409E,
+    0x4073, 0x4001, 0x7007, 0x1004,
+    0x180C, 0x0C18, 0x07F0, 0x0000};
 
-void render_bird(const Bird *bird, UINT8 *base) 
+void render_bird(const Bird *bird, UINT8 *base)
 {
     plot_16bit_bitmap((UINT16 *)base, bird->y, bird->x, bird_bitmap, BIRD_HEIGHT);
 }
 
-void render_pipe(const SetOfPipes *pipes, UINT8 *base) 
+void render_pipe(const SetOfPipes *pipes, UINT8 *base)
 {
     unsigned int bottom_pipe_y = pipes->y + PIPE_GAP_SIZE;
     unsigned int bottom_pipe_height = SCREEN_HEIGHT - bottom_pipe_y;
@@ -41,16 +40,16 @@ void render_pipe(const SetOfPipes *pipes, UINT8 *base)
     plot_rectangle((UINT32 *)base, bottom_pipe_y, pipes->x, bottom_pipe_height, PIPE_WIDTH);
 }
 
-void render_score(const Score *score, UINT8 *base) 
+void render_score(const Score *score, UINT8 *base)
 {
     char score_str[20];
 
     sprintf(score_str, "Score: %u", score->curr_score);
 
-    /*plot_string(base, 20, 20, score_str);*/
+    plot_string(base, 20, 20, score_str);
 }
 
-void render(const Model *model, UINT8 *base) 
+void render(const Model *model, UINT8 *base)
 {
     unsigned int i;
 
@@ -63,4 +62,3 @@ void render(const Model *model, UINT8 *base)
 
     render_score(&model->score, base);
 }
-
