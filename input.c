85b8bc1e5c3a6ae7718cf192218892b571b27665
gurshan23 <87831424+gurshan23@users.noreply.github.com>
2026-03-02 12:09:49 -0700

    Created input.c and input.h

---
+++ input.c
@@ -0,0 +1,12 @@
+/*
+ * File: input.c
+ *
+ * Team Members: Muji Shah, Gurshan Chera, Declan McCarthy
+ * Date Created: March 2, 2026
+ *
+ * Description:
+ * This file wraps underlying, O/S-specific code for performing keyboard and mouse input.
+ *
+ * File Status:
+ *
+*/
---
+++ input.h
@@ -0,0 +1,12 @@
+/*
+ * File: input.h
+ *
+ * Team Members: Muji Shah, Gurshan Chera, Declan McCarthy
+ * Date Created: March 2, 2026
+ *
+ * Description:
+ * Defines the input routines needed for the game.
+ *
+ * File Status:
+ *
+*/
