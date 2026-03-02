80eeb915758f091dae9f57054bccb66d2a1c3539
Declan878 <dmcca878@mtroyal.ca>
2026-03-01 19:37:21 -0700

    added a bird bitmap in renderer.c

--- renderer.c
+++ renderer.c
@@ -17,9 +17,16 @@
 
 #include "renderer.h"
 
+UINT16 bird_bitmap [16] = {
+	0x0000, 0x0000, 0x03E0, 0x0C10,
+	0x1878, 0x3088, 0x608C, 0x409E,
+	0x4073, 0x4001, 0x7007, 0x1004,
+	0x180C, 0x0C18, 0x07F0, 0x0000
+};
+
 void render_bird(const Bird *bird, UINT8 *base) 
 {
-    /*plot_16bit_bitmap((UINT16 *)base, bird->y, bird->x, bird_bitmap, BIRD_HEIGHT);*/
+    plot_16bit_bitmap((UINT16 *)base, bird->y, bird->x, bird_bitmap, BIRD_HEIGHT);
 }
 
 void render_pipe(const SetOfPipes *pipes, UINT8 *base) 
@@ -28,10 +35,10 @@ void render_pipe(const SetOfPipes *pipes, UINT8 *base)
     unsigned int bottom_pipe_height = SCREEN_HEIGHT - bottom_pipe_y;
 
     /* Top pipe */
-    /*plot_rectangle((UINT32 *)base, 0, pipes->x, pipes->y, PIPE_WIDTH);*/
+    plot_rectangle((UINT32 *)base, 0, pipes->x, pipes->y, PIPE_WIDTH);
 
     /* Bottom pipe */
-    /*plot_rectangle((UINT32 *)base, bottom_pipe_y, pipes->x, bottom_pipe_height, PIPE_WIDTH);*/
+    plot_rectangle((UINT32 *)base, bottom_pipe_y, pipes->x, bottom_pipe_height, PIPE_WIDTH);
 }
 
 void render_score(const Score *score, UINT8 *base) 
@@ -40,14 +47,12 @@ void render_score(const Score *score, UINT8 *base)
 
     sprintf(score_str, "Score: %u", score->curr_score);
 
-    /*plot_string(base, 20, 20, score_str);*/
+    plot_string(base, 20, 20, score_str);
 }
 
 void render(const Model *model, UINT8 *base) 
 {
     unsigned int i;
-
-    clear_screen((UINT32 *)base);
 
     render_bird(&model->bird, base);
 
