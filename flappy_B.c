63e0f7c026be1b61a8d7e12a7000752997e91a2a
Declan878 <dmcca878@mtroyal.ca>
2026-03-09 22:42:12 -0600

    updated main game loop and makefile

--- flappy_B.c
+++ flappy_B.c
@@ -1,27 +1,76 @@
-#include <stdio.h>
-#include <osbind.h>
-#include "model.h"
-#include "asynch.h"
-#include "synch.h"
-#include "cond.h"
-#include "raster.h"
-#include "input.h"
-
 /*
 Authors: Muji Shah, Gurshan Chera, Declan McCarthy
 Program Details: flappy bird game!
+
+File Status:
+The main game loop is implemented, but the functions 
+for processing events are not yet implemented.
+
+The input.h and .c files aren't used yet.
 */
+
+#include <stdio.h>
+#include <osbind.h>
+#include "model.h"
+#include "renderer.h"
+#include "raster.h"
+#include "asynch.h"
+#include "synch.h"
+#include "cond.h"
+#include "input.h"
+
+#define TIMER_ADDR 0x462
+
+UINT32 get_time(void);
 
 int main()
 {
     /* Main game loop: */
     Model model;
-    UINT8 *base = Physbase();
-    bool quit = false;
+    UINT8 *base = (UINT8 *)Physbase();
+    bool quit = 0; /* 0 = false, 1 = true*/
 
+    /* initializing and rendering first state */
     modelInit(&model);
     render(&model, base);
 
+    timeThen = get_time();
+
+    while (!quit)
+    {
+        if (Cconis())
+        {
+            char key = (char)Cnecin();
+            /* processAsyncEvents(&model, key) */
+        }
+
+        timeNow = get_time();
+        timeElapsed = timeNow - timeThen;
+
+        if (timeElapsed > 0)
+        {
+            /* processSyncEvents(&model) */
+
+            /* processCondEvents(&model) */
+
+            render(&model, base);
+
+            timeThen = timeNow;
+        }
+    }
 
     return 0;
+}
+
+UINT32 get_time()
+{
+    volatile UINT32 *timer = (volatile UINT32 *)TIMER_ADDR;
+    UINT32 time;
+    long old_ssp;
+
+    old_ssp = Super(0); /* enter privileged mode */
+    time = *timer; /* read timer */
+    Super(old_ssp); /* exit privileged mode */
+
+    return time;
 }
