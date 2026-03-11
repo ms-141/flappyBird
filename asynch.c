ed2e4893db60e9679b3906f0a111abccc05af3ee
Declan878 <dmcca878@mtroyal.ca>
2026-03-10 21:02:47 -0600

    removed unecessary checks from asynch.c

--- asynch.c
+++ asynch.c
@@ -7,10 +7,7 @@
  * Description:
  * This file implements the functions for handling asynchronous events, such as user input.
  *
- * File Status: 
- * Currently, the handleQuitToMenu function only sets the Gamestate to 
- * MENU, because the menu screen is not implemented.
- *
+ * File Status:
  * The handle2p is currently the same as handle1p because there is no 
  * 2 player mode implemented.
 */
@@ -18,24 +15,20 @@
 #include "asynch.h"
 
 void handleJump(Model *model) {
-    if (model->state == PLAYING)
-        model->bird.jump_ticks_remaining = 3;
+    model->bird.jump_ticks_remaining = 3;
 }
 
 void handle1p(Model *model) {
-    if (model->state == MENU)
-        modelInit(model);
+    modelInit(model);
 }
 
 /* currently the same as handle1p */
 void handle2p(Model *model) {
-    if (model->state == MENU)
-        modelInit(model);
+    modelInit(model);
 }
 
 void handleRetry(Model *model) {
-    if (model->state == GAME_OVER)
-        modelInit(model);
+    modelReset(model);
 }
 
 void handleQuitToMenu(Model *model) {
