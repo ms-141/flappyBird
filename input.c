5e03c6ebc630d8e8ab129b837eca38ea0e0aaea2
Gurshan <gcher483@mtroyal.ca>
2026-03-12 23:18:09 -0600

    Got rid of commented out old input methods

--- input.c
+++ input.c
@@ -13,28 +13,6 @@
 
 #include "input.h"
 
-/* bool processInput(Model *model) {
-    if ((model->state == PLAYING) && (model->bird.jump_ticks_remaining == 3)) {
-        return true;
-    }
-    if (model->state == MENU) {
-        return true; /* Placeholder for no input */
-  /*  }
-    if (model->state == GAME_OVER) {
-        return true; /* Placeholder for no input */
-    /*}
-    return false;
-}
-
-char nextInput(Model *model) {
-    if ((model->state == PLAYING) && (model->bird.jump_ticks_remaining == 3)) {
-        return ' '; /* Return the jump key */
-   /*}
-    if ((model->state == GAME_OVER) || (model->state == PLAYING)) {
-        return 'q'; /* End game */
-    /*}
-} */
-
 int processInput() {
     if (Cconis()) {
         return 1;
