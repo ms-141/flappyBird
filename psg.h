ea8e6e28eb9182878b870fe961cfe46f75465dc7
Gurshan <gcher483@mtroyal.ca>
2026-03-21 14:49:07 -0600

    added two prototypes to psg.h

--- psg.h
+++ psg.h
@@ -36,4 +36,10 @@ void enable_channel(int channel, int tone_on, int noise_on);
 /* Silences all PSG sound production. */
 void stop_sound();
 
+/* Loads the noise register with the given tuning. */
+void set_noise(int tuning);
+
+/* Loads the PSG envelope control registers with the given envelope shape and 16-bit sustain. */
+void set_envelope(int shape, unsigned int sustain);
+
 #endif
