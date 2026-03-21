3d4b126826546a89625e5bf47652bc43fa3328db
Gurshan <gcher483@mtroyal.ca>
2026-03-21 14:35:33 -0600

    Created PSG header file

---
+++ psg.h
@@ -0,0 +1,39 @@
+/*
+ * File: psg.h
+ *
+ * Team Members: Muji Shah, Gurshan Chera, Declan McCarthy
+ * Date Created: March 21, 2026
+ *
+ * Description:
+ * This file defines the PSG chip functions which will be used to play music and sound effects in the game.
+ * 
+ * File Status: 
+ *
+*/
+
+#ifndef PSG_H
+#define PSG_H
+
+typedef unsigned char UINT8;
+
+/* Writes the given byte value (0-255) to the given PSG register (0-15). This is a helper
+routine to be used by the other functions in this module.*/
+void write_psg(int reg, UINT8 val);
+
+/* Useful for testing purposes, but unlikely to be used in either the music or effects modules. */
+UINT8 read_psg(int reg);
+
+/* Loads the tone registers (coarse and fine) for the given channel (0=A, 1=B, 2=C) with the
+given 12-bit tuning. */
+void set_tone(int channel, int tuning);
+
+/* Loads the volume register for the given channel. */
+void set_volume(int channel, int volume);
+
+/* Turns the given channel’s tone/noise signals on/off (0=off, 1=on). */
+void enable_channel(int channel, int tone_on, int noise_on);
+
+/* Silences all PSG sound production. */
+void stop_sound();
+
+#endif
