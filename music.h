440e62f128197a1b3b7e80ce84d2efbac6fb9ed7
Gurshan <gcher483@mtroyal.ca>
2026-03-21 14:43:49 -0600

    Created music header file

---
+++ music.h
@@ -0,0 +1,27 @@
+/*
+ * File: music.h
+ *
+ * Team Members: Muji Shah, Gurshan Chera, Declan McCarthy
+ * Date Created: March 21, 2026
+ *
+ * Description:
+ * This file defines the functions for playing music in the game.
+ * 
+ * File Status: 
+ *
+*/
+
+#ifndef MUSIC_H
+#define MUSIC_H
+
+#include "psg.h"
+typedef unsigned char UINT32;
+
+/* Begins the playing of the song by loading the data for the first note into the PSG. */
+void start_music();
+
+/* Advances to the next note of the song if necessary, as determined by the amount of
+time elapsed since the previous call. */
+void update_music(UINT32 time_elapsed);
+
+#endif
