cba88b371d05f0be388ca9e9ac789f75b1e8ba4b
Declan878 <dmcca878@mtroyal.ca>
2026-03-30 23:06:01 -0600

    changed sound effects

--- effects.c
+++ effects.c
@@ -15,20 +15,21 @@
 void play_jump_effect()
 {
     set_volume(2, 16); /* enable envelope (bit 4 = 1) */
-    set_tone(2, 800);    
+    set_tone(2, 500);    
 
     /* (shape of sound, duration) */
-    set_envelope(0x0A, 1000); /* short envelope */
+    set_envelope(0x09, 1000); /* short envelope */
 
-    enable_channel(2, 1, 1); 
+    enable_channel(2, 1, 0); 
 }
 
 void play_game_over_effect()
 {
     set_volume(2, 16);     
-    set_tone(2, 800);       
+    set_tone(2, 2000);      
+    set_noise(5);
 
-    set_envelope(0x0A, 5000);  /* long envelope */
+    set_envelope(0x09, 7000);  /* long envelope */
 
-    enable_channel(2, 1, 0); 
+    enable_channel(2, 1, 1); 
 }
